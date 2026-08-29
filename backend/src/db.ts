e7b7de69a2db61da9ec6f3089f26a4d895c38777
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-29 15:34:22 +0200

    Add pg and dotenv to backend

---
+++ backend/src/db.ts
@@ -0,0 +1,9 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWD,
+	port: Number(process.env.DB_PORT),
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app.ts";
+import "dotenv/config";
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server listening at :${PORT}`);
