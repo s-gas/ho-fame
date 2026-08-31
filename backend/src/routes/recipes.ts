c544003d637b2a037bdc7726f88d3f58b7080b1e
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-31 20:39:56 +0200

    Create endpoint for posting new entries

--- backend/src/routes/recipes.ts
+++ backend/src/routes/recipes.ts
@@ -5,7 +5,13 @@ const router = Router();
 
 router.get("/", async (_req, res) => {
   const entries = await recipesService.getAll();
-  return res.json(entries);
+  console.log(entries);
+  return res.status(200).json(entries);
+});
+
+router.post("/", async (req, res) => {
+  const entry = await recipesService.createRecipe(req.body);
+  return res.status(201).json(entry);
 })
 
 export default router;
--- backend/src/services/recipes.ts
+++ backend/src/services/recipes.ts
@@ -1,8 +1,16 @@
 import pool from "../db.ts";
+import type { Recipe } from "../types.ts";
 
 const getAll = async () => {
   const result = await pool.query("SELECT * FROM recipes");
   return result.rows;
 };
 
-export default { getAll };
+const createRecipe = async (recipe: Recipe) => {
+  const result = await pool.query(`
+    INSERT INTO recipes (name) VALUES ($1) RETURNING *;
+  `, [recipe.name]);
+  return result.rows[0];
+}
+
+export default { getAll, createRecipe };
