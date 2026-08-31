8ea4fba5bcee92e2e120f09b0e964bc9d0aa84fb
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-31 14:07:15 +0200

    Log the database in use

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,10 @@ import app from "./app.ts";
 import "dotenv/config";
 
 const PORT = process.env.PORT;
+const DB = process.env.DB_NAME;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at :${PORT}`);
+  console.log("Server info:");
+  console.log("- Port:\t", PORT);
+  console.log("- DB:\t", DB);
 });
